981abe98d4c1ec9a8f1e17c73ed8f635eb6e845f
Eduardo Rs <rodrigueseduardo1966@gmail.com>
2022-02-20 13:27:47 -0300

    Reformulei tudo...

--- matriz.py
+++ matriz.py
@@ -1,168 +1,84 @@
 from __future__ import annotations
 
-from typing import List, Iterable
+from typing import List, TypeVar
+from random import randint
+
+from tabulate import tabulate
 
 List_Matriz = List[List[int]]
-
+T = TypeVar('T', bound='Matriz')
 
 class Matriz:
-    def __init__(self, matriz: List_Matriz = None):
-        self.matriz = matriz
-        self.matriz_x: List_Matriz = []
+    def __init__(self, matriz: List_Matriz = None) -> None:
+        self._matriz = matriz
 
-    def __repr__(self) -> str:
-        return f"<Matriz order='{self.get_order(self.matriz)}'>"
+    def __new__(self, matriz: List_Matriz = None) -> Matriz:
+        return super().__new__(self)
 
-    def __str__(self) -> str:
-        return self.show_matriz()
-
-    def __add__(self, other) -> Matriz:
+    def __add__(self, other: T) -> Matriz:
+        matriz = self._matriz
+        matriz_z = []
+        get_order = Matriz.get_order
         if isinstance(other, Matriz):
-            soma = self.sum(self.matriz, other.matriz)
-            return Matriz(soma)
+            if get_order(matriz) == get_order(other._matriz):
+                for i in range(len(matriz)):
+                    matriz_z.append([])
+                    for j in range(len(matriz[i])):
+                        matriz_z[i].append(matriz[i][j] + other._matriz[i][j])
+                return Matriz(matriz=matriz_z)
+            else:
+                raise TypeError("As matrizes não possuem a mesma ordem. Impossível somar.")
         else:
-            raise TypeError(
-                "Não é possivel somar, por causa do tipo das matrizes diferentes"
-            )
+            raise TypeError(f"unsupported operand type(s) for +: 'Matriz' and '{other.__class__.__name__}'")
 
-    def __sub__(self, other) -> Matriz:
+    def __sub__(self, other: T) -> Matriz:
+        matriz = self._matriz
+        matriz_z = []
+        get_order = Matriz.get_order
         if isinstance(other, Matriz):
-            sub = self.subtract(self.matriz, other.matriz)
-            return Matriz(sub)
+            if get_order(matriz) == get_order(other._matriz):
+                for i in range(len(matriz)):
+                    matriz_z.append([])
+                    for j in range(len(matriz[i])):
+                        matriz_z[i].append(matriz[i][j] - other._matriz[i][j])
+                return Matriz(matriz=matriz_z)
+            else:
+                raise TypeError("As matrizes não possuem a mesma ordem. Impossível subtrair.")
         else:
-            raise TypeError(
-                "Não é possivel subtrair, por causa do tipo das matrizes diferentes"
-            )
+            raise TypeError(f"unsupported operand type(s) for -: 'Matriz' and '{other.__class__.__name__}'")
 
-    def __mul__(self, other) -> Matriz:
+
+    def __mul__(self, other: T) -> List[Matriz, str]:
+        matriz = self._matriz
+        matriz_z = []
         if isinstance(other, Matriz):
-            multi = self.multiply(self.matriz, other.matriz)
-            return [Matriz(multi[0]), multi[1]]
+            if len(matriz[0]) == len(other._matriz):
+                txt = ""
+                for i in range(len(matriz)):
+                    matriz_z.append([])
+                    for j in range(len(other._matriz[0])):
+                        matriz_z[i].append(0)
+                        for k in range(len(matriz[i])):
+                            txt += f"Z{i+1}{j+1} += {matriz[i][k]} * {other._matriz[k][j]} = {matriz[i][k] * other._matriz[k][j]}\n"
+                            matriz_z[i][j] += matriz[i][k] * other._matriz[k][j]
+                return [Matriz(matriz=matriz_z), txt]
+            else:
+                raise TypeError(f"As matriz 1 tem {len(matriz[0])} colunas e a matriz 2 tem {len(other._matriz[0])} linhas. Impossível multiplicar.")
         else:
-            raise TypeError(
-                "Não é possivel multiplicar, por causa do tipo das matrizes diferentes"
-            )
+            raise TypeError(f"unsupported operand type(s) for *: 'Matriz' and '{other.__class__.__name__}'")
 
     @classmethod
-    def create_matriz_generic(
-        cls, rows: int, cols: int, name: str, show: bool = False
-    ):
+    def generate_random_matriz(cls, row: int, col: int) -> Matriz:
         matriz = []
-        for i in range(rows):
+        for i in range(row):
             matriz.append([])
-            for j in range(cols):
-                if show:
-                    matriz[i].append(f"{name}{i+1}{j+1}")
-                else:
-                    matriz[i].append(f"{i+1}{j+1}")
-        return cls(matriz)
+            for j in range(col):
+                matriz[i].append(randint(-100,100))
+        return cls(matriz=matriz)
 
-    def show_matriz(self) -> str:
-        txt = ""
-        try:
-            len(self.matriz[0])
-        except TypeError as e:
-            if str(e) == "object of type 'int' has no len()":
-                for i in range(len(self.matriz)):
-                    txt += f"{self.matriz[i]}\n"
-                return txt
-            else:
-                raise e
-        for i in range(len(self.matriz)):
-            for j in range(len(self.matriz[i])):
-                txt += str(self.matriz[i][j]) + " "
-            txt += "\n"
-        return txt
-
-    def get_column(self, col: int) -> List_Matriz:
-        column = []
-        for i in range(len(self.matriz)):
-            column.append(self.matriz[i][col - 1])
-        return column
-
-    def get_row(self, row: int) -> List_Matriz:
-        return self.matriz[row - 1]
-
-    def check_to_multiply(
-        self, matriz_x: List_Matriz, matriz_y: List_Matriz
-    ) -> bool:
-        if len(matriz_x[0]) == len(matriz_y):
-            return True
-        else:
-            return False
-
-    def check_to_sum(
-        self, matriz_x: List_Matriz, matriz_y: List_Matriz
-    ) -> bool:
-        if self.get_order(matriz_x) == self.get_order(matriz_y):
-            return True
-        else:
-            return False
-
-    def sum(self, matriz_x: List_Matriz, matriz_y: List_Matriz) -> Matriz:
-        if self.check_to_sum(matriz_x, matriz_y):
-            matriz_z = list()
-            for i in range(len(matriz_x)):
-                matriz_z.append([])
-                for j in range(len(matriz_x)):
-                    matriz_z[i].append(matriz_x[i][j] + matriz_y[i][j])
-            return matriz_z
-        else:
-            return TypeError(
-                "Não é possivel somar, por causa das ordens das matrizes diferentes"
-            )
-
-    def subtract(self, matriz_x: List_Matriz, matriz_y: List_Matriz) -> Matriz:
-        if self.check_to_sum(matriz_x, matriz_y):
-            matriz_z = []
-            for i in range(len(matriz_x)):
-                matriz_z.append([])
-                for j in range(len(matriz_x[0])):
-                    matriz_z[i].append(matriz_x[i][j] - matriz_y[i][j])
-            return matriz_z
-        else:
-            raise TypeError(
-                "Não é possivel subtrair, por causa das ordens das matrizes diferentes"
-            )
-
-    def multiply_matriz(self, num: int) -> Matriz:
-        matriz_z = []
-        for i in range(len(self.matriz)):
-            matriz_z.append([])
-            for j in range(len(self.matriz[i])):
-                matriz_z[i].append(self.matriz[i][j] * num)
-        return matriz_z
-
-    def get_matriz_transpose(self):
-        matriz_transpose = []
-        for i in range(len(self.matriz[0])):
-            matriz_transpose.append([])
-            for j in range(len(self.matriz)):
-                matriz_transpose[i].append(self.matriz[j][i])
-        return matriz_transpose
-
-    @property
-    def T(self):
-        return self.get_matriz_transpose()
-
-    def multiply(self, matriz_x: List_Matriz,
-                 matriz_y: List_Matriz) -> Iterable[Matriz | str]:
-        if self.check_to_multiply(matriz_x, matriz_y):
-            matriz_z = []
-            txt = ""
-            for i in range(len(matriz_x)):
-                matriz_z.append([])
-                for j in range(len(matriz_y[0])):
-                    matriz_z[i].append(0)
-                    for k in range(len(matriz_y)):
-                        txt += f"Z{i+1}{j+1} += {matriz_x[i][k]} * {matriz_y[k][j]} = {matriz_x[i][k] * matriz_y[k][j]}\n"
-                        matriz_z[i][j] += matriz_x[i][k] * matriz_y[k][j]
-            return [matriz_z, txt]
-        else:
-            return None
 
     @staticmethod
-    def get_order(matriz: Matriz) -> str:
+    def get_order(matriz: List_Matriz) -> str:
         try:
             len(matriz[0])
         except TypeError as e:
@@ -170,5 +86,58 @@ class Matriz:
                 return f"{len(matriz)} x 1"
         return f"{len(matriz)} x {len(matriz[0])}"
 
-    def get_element(self, row: int, col: int) -> int:
-        return self.matriz[row - 1][col - 1]
+    def show_matriz(self) -> str:
+        txt = ""
+        matriz = self._matriz
+        if matriz:
+            try:
+                len(matriz[0])
+            except TypeError as e:
+                if str(e) == "object of type 'int' has no len()":
+                    for i in range(len(matriz)):
+                        txt += f"{matriz[i]}\n"
+                    return txt
+                else:
+                    raise e
+            for i in range(len(matriz)):
+                for j in range(len(matriz[i])):
+                    txt += str(matriz[i][j]) + " "
+                txt += "\n"
+            return tabulate(matriz, tablefmt="fancy_grid")
+        else:
+            raise TypeError("A matriz não foi definida.")
+
+    def transpose(self) -> Matriz:
+        matriz = self._matriz
+        if matriz:
+            try:
+                len(matriz[0])
+            except TypeError as e:
+                if str(e) == "object of type 'int' has no len()":
+                    return matriz
+                else:
+                    raise e
+            new_matriz = []
+            for i in range(len(matriz[0])):
+                new_matriz.append([])
+                for j in range(len(matriz)):
+                    new_matriz[i].append(matriz[j][i])
+            return Matriz(new_matriz)
+        else:
+            raise TypeError("A matriz não foi definida.")
+
+    @property
+    def T(self) -> Matriz:
+        return self.transpose()
+
+    def multiply_matriz(self, num: int) -> Matriz:
+        matriz = self._matriz
+        matriz_z = []
+        if matriz:
+            for i in range(len(matriz)):
+                matriz_z.append([])
+                for j in range(len(matriz[i])):
+                    matriz_z[i].append(matriz[i][j] * num)
+            return Matriz(matriz=matriz_z)
+        else:
+            raise TypeError("A matriz não foi definida.")
